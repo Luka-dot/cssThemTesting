cc2219e5a70916638be12e647cd2192430669ffb
Luka-dot <79lukask@gmail.com>
2020-09-04 19:44:07 -0400

    style added to new navbar

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const themeSwitch = document.querySelector('input');
 themeSwitch.addEventListener('change', () => {
   document.body.classList.toggle('dark-theme');
 });
+
+
+const sections = [
+    selectElementByClass('home'),
+    selectElementByClass('about'),
+    selectElementByClass('services'),
+    selectElementByClass('downloads'),
+    selectElementByClass('contact'),
+  ];
+  
+  const navItems = {
+    home: selectElementByClass('homeNavItem'),
+    about: selectElementByClass('aboutNavItem'),
+    services: selectElementByClass('servicesNavItem'),
+    downloads: selectElementByClass('downloadsNavItem'),
+    contact: selectElementByClass('contactNavItem'),
+  };
+
+function selectElementByClass(className) {
+    return document.querySelector(`.${className}`);
+  }
+
