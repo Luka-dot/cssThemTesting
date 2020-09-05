bd0fbdfc9a39bcda3b39fb9a50abcd4c24a8649d
Luka-dot <79lukask@gmail.com>
2020-09-05 12:12:19 -0400

    new test app added animations-test

--- animations-test/src/App.js
+++ animations-test/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import { fade } from 'react-awesome-reveal';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Just a test txt</h2>
     </div>
   );
 }
