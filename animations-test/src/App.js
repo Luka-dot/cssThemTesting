5064ac1d5564f9bc32de664b5c8727355c65e7f9
Luka-dot <79lukask@gmail.com>
2020-09-05 16:08:35 -0400

    animation test divs added

--- animations-test/src/App.js
+++ animations-test/src/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import { fade } from 'react-awesome-reveal';
+import { Fade, Bounce, Flip, Hinge, JackInTheBox, Roll, Rotate, Slide, Zoom } from 'react-awesome-reveal';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Just a test txt</h2>
+      <Fade>
+        <p>I will gently appear as I enter the viewport</p>
+      </Fade>
+      <div className="spacer">
+        
+      </div>
+      <Bounce>
+        <p>Testing bounce effect</p>
+      </Bounce>
+      <div className="spacer">
+        
+      </div>
+      <Flip>
+        <h3>Fliiiip</h3>
+      </Flip>
+      <div className="spacer">
+        
+      </div>
+      <JackInTheBox>
+        <h3>JackInTheBox</h3>
+      </JackInTheBox>
+      <div className="spacer">
+        
+      </div>
+      <Roll>
+        <h3>ROLL</h3>
+      </Roll>
+      <div className="spacer">
+        
+      </div>
+      <Rotate>
+        <h3>ROTATE</h3>
+      </Rotate>
+      <div className="spacer">
+        
+      </div>
+      <Zoom>
+        <h3>ZOOM</h3>
+      </Zoom>
     </div>
   );
 }
